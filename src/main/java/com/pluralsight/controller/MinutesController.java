403d978c091bb1481775590ffa30dbdc47ab18ea
Jerry Hu <hh201020@gmail.com>
2016-10-14 16:35:22 -0400

    Take out service tier.

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -3,6 +3,7 @@ package com.pluralsight.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pluralsight.model.Activity;
 import com.pluralsight.model.Exercise;
+import com.pluralsight.service.ExerciseService;
 
 @Controller
 public class MinutesController {
+	
+	@Autowired
+	private ExerciseService exerciseService;
 	
 	@RequestMapping(value = "/addMinutes")  // the actual url provided should be: http://localhost:8070/FitnessTracker/addMinutes.html
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise){
@@ -34,20 +39,7 @@ public class MinutesController {
 	
 	@RequestMapping(value = "/activities", method = RequestMethod.GET) 
 	public @ResponseBody List<Activity> findAllActivities(){
-		List<Activity> activities = new ArrayList<>();
-		
-		Activity run = new Activity();
-		run.setDesc("Run");
-		activities.add(run);
-		
-		Activity bike = new Activity();
-		bike.setDesc("bike");
-		activities.add(bike);
-		
-		Activity swim = new Activity();
-		swim.setDesc("swim");
-		activities.add(swim);
-		
-		return activities;
+
+		return exerciseService.findAllActivities();
 	}
 }
