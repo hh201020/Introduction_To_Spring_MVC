37b66cc660f51321ee09d5a453fb00d441ffc6bd
Jerry Hu <hh201020@gmail.com>
2016-10-18 12:33:47 -0400

    Named Queries put all queries on Entity, making code tidy.

--- src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
@@ -26,17 +27,19 @@ public class GoalRepositoryImpl implements GoalRepository {
 	@SuppressWarnings({"unchecked","rawtypes"})
 	@Override
 	public List<Goal> loadAll() {
-		Query query = em.createQuery("Select g from Goal g");
+		//Query query = em.createQuery("Select g from Goal g");
+		TypedQuery<Goal> query = em.createNamedQuery(Goal.FIND_ALL_GOALS, Goal.class);
 		List goals = query.getResultList();
 		
 		return goals;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<GoalReport> findAllGoalReports() {
-		Query query = em.createQuery("Select new com.pluralsight.model.GoalReport(g.minutes, e.minutes, e.activity) " + 
-				"from Goal g, Exercise e where g.id = e.goal.id" );
+		//Query query = em.createQuery("Select new com.pluralsight.model.GoalReport(g.minutes, e.minutes, e.activity) " + 
+		//		"from Goal g, Exercise e where g.id = e.goal.id" );
+		
+		TypedQuery<GoalReport> query = em.createNamedQuery(Goal.FIND_GOAL_REPORTS, GoalReport.class);
 		return query.getResultList();
 	}
 }
