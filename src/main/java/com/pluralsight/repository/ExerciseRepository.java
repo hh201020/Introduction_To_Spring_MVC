7cc3a1b5c7b93683a01f5c04affe448818fba9c0
Jerry Hu <hh201020@gmail.com>
2016-10-18 13:38:57 -0400

    First Using Spring Data JPA

--- src/main/java/com/pluralsight/repository/ExerciseRepository.java
+++ src/main/java/com/pluralsight/repository/ExerciseRepository.java
@@ -1,7 +1,10 @@
 package com.pluralsight.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.pluralsight.model.Exercise;
 
-public interface ExerciseRepository {
-	Exercise save(Exercise exercise);
+@Repository("exerciseRepository")
+public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
 }
