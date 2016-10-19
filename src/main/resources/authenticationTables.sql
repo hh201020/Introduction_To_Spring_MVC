f0445ea9ecc4ba03efef9f3ec7173ebcd4fdffb3
Jerry Hu <hh201020@gmail.com>
2016-10-19 10:33:35 -0400

    BCrypt Hash password

--- src/main/resources/authenticationTables.sql
+++ src/main/resources/authenticationTables.sql
@@ -21,3 +21,9 @@ ENGINE=InnoDB
 insert into users (username, password, enabled) values ("b", "s", true)
 
 insert into authorities (username, authority) values ("b", "ROLE_USER")
+
+update users set password="5ebe2294ecd0e0f08eab7690d2a6ee69" where username="Bryan"
+
+alter table users modify password varchar(60);
+
+update users set password="$2a$10$5XbiJjvz95XL6Ed5uP71/eCwNT6RkhqhvUqEUdDJKt4QxWE345fsO" where username="Bryan"
--- src/test/java/PasswordHash.java
+++ src/test/java/PasswordHash.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import junit.framework.TestCase;
 
@@ -9,6 +10,13 @@ public class PasswordHash extends TestCase {
 		String password = "s";
 		Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
 		String hashedPassword = passwordEncoder.encodePassword(password, null);
+		System.out.println(hashedPassword);
+	}
+	
+	public void testBCryptHash(){
+		String password = "s";
+		BCryptPasswordEncoder passwordEncode = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncode.encode(password);
 		System.out.println(hashedPassword);
 	}
 	
